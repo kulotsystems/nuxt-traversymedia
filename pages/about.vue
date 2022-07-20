d8c352d0bbe9bd253913a338dd7a53e1e9cfa21f
Arvic S. Babol <kulotsystems@gmail.com>
2022-07-20 21:32:13 +0800

    Create About page.

---
+++ pages/about.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>About DadJokes</h1>
+        <p>This is an app that displays corny dad jokes</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'about',
+        head() {
+            return {
+                title: 'About Us',
+                meta : [
+                    {
+                        hid    : 'description',
+                        name   : 'description',
+                        content: 'Best place for corny dad jokes'
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
