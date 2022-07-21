ab6aa94651d967fabce877d5e74442a70cbe730a
Arvic S. Babol <kulotsystems@gmail.com>
2022-07-21 11:00:47 +0800

    Create JokeSearch component.

---
+++ components/JokeSearch.vue
@@ -0,0 +1,27 @@
+<template>
+    <form @submit.prevent="searchJoke">
+        <input type="text" v-model="text" placeholder="Search Jokes...">
+        <input type="submit" value="Search Jokes">
+    </form>
+</template>
+
+<script>
+    export default {
+        name: 'JokeSearch',
+        data() {
+            return {
+                text: ''
+            }
+        },
+        methods: {
+            searchJoke() {
+                this.$emit('search-joke', this.text);
+                this.text = '';
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
