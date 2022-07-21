c16c83a645d59450a290006bcf476d732445d022
Arvic S. Babol <kulotsystems@gmail.com>
2022-07-21 10:03:27 +0800

    Fetch jokes from API.

--- pages/jokes/index.vue
+++ pages/jokes/index.vue
@@ -5,8 +5,28 @@
 </template>
 
 <script>
+
     export default {
         name: 'Jokes.Index',
+        data() {
+            return {
+                jokes: []
+            }
+        },
+        async created() {
+            const config = {
+                headers: {
+                    'Accept': 'application/json'
+                }
+            }
+            try {
+                const res = await this.$axios.get('https://icanhazdadjoke.com/search', config);
+                this.jokes = res.data.results;
+            }
+            catch(err) {
+               console.log('ERROR: ', err);
+            }
+        },
         head() {
             return {
                 title: 'Dad Jokes',
