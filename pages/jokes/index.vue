294530eecbe65e0963bde0c79f66e8be99987c3a
Arvic S. Babol <kulotsystems@gmail.com>
2022-07-21 09:11:36 +0800

    Create Jokes page.

---
+++ pages/jokes/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        jokes
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Jokes.Index',
+        head() {
+            return {
+                title: 'Welcome to Dad Jokes',
+                meta : [
+                    {
+                        hid    : 'description',
+                        name   : 'description',
+                        content: 'Best place for corny dad jokes'
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
